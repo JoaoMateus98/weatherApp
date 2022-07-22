90a739ba726ecabd0aeebe2595b2d8ba92b90b5f
JoaoMateus98 <joaobsu98@gmail.com>
2022-07-22 18:22:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+class Weather {
+  constructor() {
+    this.WEATHER_KEY = "6f60271f6f33f4cdabaca4fdb80f42fd";
+    this.description = "no description";
+    this.temp = "no temp";
+    this.feelsLike = "no feels like";
+    this.humidity = "no humidity";
+    this.windspeed = "no wind";
+  }
+
+  async getWeather(city, units) {
+    const displayedUnits = units === "Imperial" ? "F" : "C";
+    const displayedWindSpeed = units === "Imperial" ? " mph" : " km/h";
+    try {
+      const response =
+        await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${this.WEATHER_KEY}
+        `);
+      const responseJson = await response.json();
+      console.log(responseJson);
+      this.description = {
+        name: "description",
+        data: responseJson.weather[0].description,
+      };
+      this.temp = {
+        name: "temperature",
+        data: responseJson.main.temp + displayedUnits,
+      };
+      this.feelsLike = {
+        name: "feels like",
+        data: responseJson.main.feels_like,
+      };
+      this.humidity = {
+        name: "humidity",
+        data: responseJson.main.humidity,
+      };
+      this.windspeed = {
+        name: "wind speed",
+        data: responseJson.wind.speed + displayedWindSpeed,
+      };
+    } catch {
+      console.log("failed to retrive data");
+    }
+
+    return [
+      this.description,
+      this.temp,
+      this.feelsLike,
+      this.humidity,
+      this.windspeed,
+    ];
+  }
+}
+
+const weatherClass = new Weather();
+
+function getNewWeather(city = "Rockland", units = "Imperial") {
+  weatherClass.getWeather(city, units).then((arr) => {
+    arr.forEach((val) => {
+      console.log(val);
+    });
+  });
+}
+
+getNewWeather();
